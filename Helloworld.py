973bb2d438dc85207fdbfeb54599f489b8c9b573
mohansinghs <mlshekhawat@gmail.com>
2019-02-22 19:31:00 +0530

    Adding Hello File

---
+++ Helloworld.py
@@ -0,0 +1,7 @@
+print('{0} Welcome to python {1} is teaching me python'.format('Jilani', 'Mohan'))
+print((5 + 5) * 5)
+test = 5
+move = 10
+print ('test {0}'.format(test))
+print ('move {0}'.format(move))
+print (test + move)
