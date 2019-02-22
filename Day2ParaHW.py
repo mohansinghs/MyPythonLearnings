737f00234a79a063c62d8b33914c6c6ff757e102
mohansinghs <mlshekhawat@gmail.com>
2019-02-22 19:26:10 +0530

    initial commit

---
+++ Day2ParaHW.py
@@ -0,0 +1,6 @@
+name = input('Enter Your Name:- ')
+sbu = input('SBU:- ')
+role = input('role:- ')
+employeeid = input ('Employee ID:- ')
+experience = input('Experience:- ')
+print('Hello {0} Wellcome to universe Ltd. We are happy to know that you have {1} expreience. You will be part of {2} SBU & your role will be {3}. Your Employee ID in system is {4}'.format(name,experience,sbu,role,employeeid))
