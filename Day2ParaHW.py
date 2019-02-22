9ff32321f8632bbc0f3c6e814296b2331494508d
mohansinghs <mlshekhawat@gmail.com>
2019-02-22 19:38:58 +0530

    File Formatting done for better readability

--- Day2ParaHW.py
+++ Day2ParaHW.py
@@ -3,4 +3,5 @@ sbu = input('SBU:- ')
 role = input('role:- ')
 employeeid = input ('Employee ID:- ')
 experience = input('Experience:- ')
-print('Hello {0} Wellcome to universe Ltd. We are happy to know that you have {1} expreience. You will be part of {2} SBU & your role will be {3}. Your Employee ID in system is {4}'.format(name,experience,sbu,role,employeeid))
+print('Hello {0} Wellcome to universe Ltd. We are happy to know that you have {1} expreience. You will be part of {2} '
+      'SBU & your role will be {3}. Your Employee ID in system is {4}'.format(name,experience,sbu,role,employeeid))
