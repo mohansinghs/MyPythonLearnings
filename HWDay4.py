c8220e507b1e042d695d307a80843e34d52020b2
mohansinghs <mlshekhawat@gmail.com>
2019-02-24 14:09:49 +0530

    Displaying the values entered by user

---
+++ HWDay4.py
@@ -0,0 +1,6 @@
+print('Enter your details in below fields')                       # quotes used for printing instruction for user
+name = input('Enter Name:- ')                                     # always use for getting information from user
+sbu = input('Enter SBU:- ')
+print('Information entered by you are as follow')
+print('Name:-',name)
+print('SBU:-',sbu)
