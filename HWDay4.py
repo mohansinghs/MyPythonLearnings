09b68454d0ad4ebc699181837c9f978e4b39d812
mohansinghs <mlshekhawat@gmail.com>
2019-02-25 15:42:19 +0530

    Displaying the values entered by user

--- HWDay4.py
+++ HWDay4.py
@@ -1,9 +1,8 @@
 print('Enter your details in below fields')                       # quotes used for printing instruction for user
 name = input('Enter Name:- ')                                     # always use for getting information from user
 sbu = input('Enter SBU:- ')
-print('Information entered by you are as follow')
-print('Name:-',name)
-print('SBU:-',sbu)
 print('If you want to change SBU please enter new SBU')
-sbu1 = input('Enter New SBU:- ')
-print('New SBU is {0} & your old SBU was {1}'.format(sbu1,sbu))
+sbu = input('Enter New SBU:- ')
+print('Your SBU is changed successfully check below')
+print('Name:- {0}'.format(name))
+print('SBU:-{0}'.format(sbu))                                       # to override never create new input
